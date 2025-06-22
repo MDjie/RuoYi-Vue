a4bbddaf1cffde3d459511b32b5ebcaa8d6f8112
MDjie <3228405267@qq.com>
2025-06-22 23:10:00 +0800

    修改index页面

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="annotation-info-container">
     <h2 class="main-title">《HSC：Hierarchy-Aware Spurious Correlation Benchmarking for Text Classification Systems》</h2>
-    <p class="subtitle">我们对下列 <b>8 个数据集</b>分别采样了单个版本的人工测试版：</p>
+    <p class="subtitle">我们对下列 <b>8 个数据集</b>分别采样划分了多个人工测试子集：</p>
     <p class="desc">
       <span class="dataset-list">Ag_News、Amazon、Civil_Comment、ED、IMDB、Rotten_Tomato、Tweet、Yahoo。</span>
-      其中每个数据集使用 <b>500-3000</b> 条不等的数据构成人工测试数据集，内部标签划分比较平均模拟全环境测试环境。数据集名称后标注为了合理保证人工标注的质量要求需要达到的最低准确率，若不合格默认认该数据集的人工标注质量低、不认真，最后结果不予采纳，最后的答案视为无效。
+      其中每个子数据集使用 <b>350</b> 条数据构成人工测试数据集，内部标签划分比较平均模拟全环境测试环境。数据集名称后标注为了合理保证人工标注的质量要求需要达到的最低准确率，若不合格默认认该数据集的人工标注质量低、不认真，最后结果不予采纳，最后的答案视为无效。
     </p>
     <div class="table-image-row">
       <el-table :data="tableData" border style="width: 100%; max-width: 420px; flex: 1;">
         <el-table-column prop="name" label="数据集" align="center" width="120" />
         <el-table-column prop="total" label="总数" align="center" width="80" />
         <el-table-column prop="classNum" label="分类数" align="center" width="80" />
-        <el-table-column prop="acc" label="最低 ACC 要求" align="center" width="120" />
+        <!-- <el-table-column prop="acc" label="最低 ACC 要求" align="center" width="120" /> -->
       </el-table>
-      <img src="@/assets/images/tinghua.png" alt="tinghua" class="side-image" width="100%" height="100%" />
           </div>
     <div class="reward-section">
       <b>奖励可选：</b>
@@ -32,14 +31,14 @@ export default {
   data() {
     return {
       tableData: [
-        { name: 'Ag_News', total: 2000, classNum: 4, acc: '95%' },
-        { name: 'Amazon_En', total: 2000, classNum: 5, acc: '70%' },
-        { name: 'Civil_Comment', total: 3000, classNum: 2, acc: '95%' },
-        { name: 'ED', total: 500, classNum: 32, acc: '10%' },
-        { name: 'IMDB', total: 2000, classNum: 2, acc: '95%' },
-        { name: 'Rotten', total: 500, classNum: 2, acc: '90%' },
-        { name: 'Tweet', total: 2000, classNum: 20, acc: '30%' },
-        { name: 'Yahoo', total: 3000, classNum: 10, acc: '70%' },
+        { name: 'Ag_News', total:"350*4" , classNum: 4, acc: '90%' },
+        { name: 'Amazon', total: "350*5", classNum: 5, acc: '%' },
+        { name: 'Civil_Comment', total: "350*3", classNum: 2, acc: '85%' },
+        { name: 'ED', total: "350*4", classNum: 32, acc: '%' },
+        { name: 'IMDB', total: "350*5", classNum: 2, acc: '95%' },
+        { name: 'Rotten', total: "350*4", classNum: 2, acc: '%' },
+        { name: 'Tweet', total: "350*5", classNum: 20, acc: '%' },
+        { name: 'Yahoo', total: "350*5", classNum: 10, acc: '72%' },
       ]
     }
   }
